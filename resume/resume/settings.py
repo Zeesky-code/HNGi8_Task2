1e501b21a9a13139bd29dca685a98d8a629270c9
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-19 21:44:31 +0300

    installed whitenoise

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -19,12 +19,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'crispy_forms',
+    'whitenoise'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
